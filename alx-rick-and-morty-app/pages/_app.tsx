35e5a438aa13321873c26a348dd37664b553aeb6
Elite6802 <kelvimutinda347@gmail.com>
2025-11-13 15:05:49 +0300

    Updates

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -3,11 +3,13 @@ import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client/react";
 import client from "../graphql/apolloClient";
 import ErrorBoundary from '@/components/ErrorBoundary';
+import ErrorProneComponent from '@/components/ErrorProneComponent';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary>
       <Component {...pageProps} />
+      <ErrorProneComponent />
     </ErrorBoundary>
   )
 }
